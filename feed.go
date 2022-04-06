608633039ceeef00f897550bcdd579b086aa638f
Evgeny Kuznetsov <evgeny@kuznetsov.md>
2022-04-06 22:05:19 +0300

    fix err handling

--- feed.go
+++ feed.go
@@ -137,7 +137,7 @@ func downloadFile(fs afero.Fs, filenamePrefix, url string) (string, error) {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return false, err
+		return "", err
 	}
 	req.Header.Add("User-Agent", `Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.27 Safari/537.36`)
 	resp, err := client.Do(req)
